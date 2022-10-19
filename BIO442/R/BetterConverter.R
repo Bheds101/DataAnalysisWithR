a30647cb459d4517b407c6dada45f1dd47541d1c
Bheds101 <hedstrom.ben@gmail.com>
2022-10-19 17:22:21 -0400

    first commit

---
+++ BIO442/R/BetterConverter.R
@@ -0,0 +1,19 @@
+#' Latitude and Longitude Converter
+#' 
+#' This function takes latitude and longitude from minute format and converts to degrees; function requires parzer package 
+#' @param input the vector of information to convert 
+#' @param type The type of data being entered ("lat" or "lon")
+#' @keywords latitude, longitude 
+
+
+BetterConverter <- function(input, type){
+  require(parzer)
+  if (type == 'lat'){
+    x <- parse_lat(input)
+  }
+  if (type == 'lon'){
+    x <- parse_lon(input)
+  }
+  return(x)
+}
+
